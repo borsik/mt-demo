9359fd3f8a4c70b88c46376932672786a10cdbd7
Olzhas Aitkaliyev <oaitkaliyev@innodata.ru>
2018-08-13 23:07:25 +0300

    account route test

--- src/test/scala/RouteSpec.scala
+++ src/test/scala/RouteSpec.scala
@@ -1,4 +1,6 @@
+import akka.http.scaladsl.model.{HttpEntity, HttpMethods, HttpRequest, MediaTypes}
 import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.util.ByteString
 import org.scalatest._
 
 class RouteSpec extends FlatSpec with Matchers with ScalatestRouteTest with RouteHandler {
@@ -6,6 +8,24 @@ class RouteSpec extends FlatSpec with Matchers with ScalatestRouteTest with Rout
   "Get request" should "return Say hello" in {
     Get("/hello") ~> route ~> check {
       responseAs[String] shouldEqual "<h1>Say hello to akka-http</h1>"
+    }
+  }
+
+  it should "return Account" in {
+    val json = """{
+                 |	"name": "John Doe",
+                 |	"amount": 200
+                 |}""".stripMargin
+
+    val postRequest = HttpRequest(
+      HttpMethods.POST,
+      uri = "/create",
+      entity = HttpEntity(MediaTypes.`application/json`, ByteString(json)))
+
+    postRequest ~> route ~> check {
+      val account = responseAs[Account]
+      account.name shouldEqual "John Doe"
+      account.amount shouldEqual 200
     }
   }
 
